a81685f6716aade2ab62ec4d8d0b466fc716e878
pat <tuanpa.tpa@gmail.com>
2023-03-27 09:20:14 +0700

    Initial commit

---
+++ src/main/java/com/example/onetimepassword/OneTimePasswordApplication.java
@@ -0,0 +1,13 @@
+package com.example.onetimepassword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneTimePasswordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneTimePasswordApplication.class, args);
+    }
+
+}
